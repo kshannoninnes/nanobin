e6963761fc3430761091bd0116eb3b2fe74e579e
Kalon Shannon-Innes <k.shannoninnes@gmail.com>
2025-07-10 19:42:33 +1000

    Fix bug in string constraint

--- Components/Models/Paste.cs
+++ Components/Models/Paste.cs
@@ -6,7 +6,7 @@ public class Paste(string content)
 {
     public string Id { get; init; } = Guid.NewGuid().ToString("N")[..18];
     // ReSharper disable once EntityFramework.ModelValidation.UnlimitedStringLength
-    public string Content { get; init; } = content[..100000];
+    public string Content { get; init; } = new(content.Take(100000).ToArray());
     public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
 
     public string GetFormattedTimestamp()
